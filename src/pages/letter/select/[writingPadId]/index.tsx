d84b5e052cd18cef2b41b31fdb011fea70f72734
xeo <xeo.seo@companywe.co.kr>
2024-05-09 19:52:42 +0900

    fix : 에러가 롼련 수정정

--- src/pages/letter/select/[writingPadId]/index.tsx
+++ src/pages/letter/select/[writingPadId]/index.tsx
@@ -1,9 +1,6 @@
 import Select from '@/components/views/letter/Select';
 import { useRouter } from 'next/router';
 
-export default function Page({ params }: { params: { writingPadId: string } }) {
-  const router = useRouter();
-
-  const writingPadId = router.query.writingPadId as string;
-  return <Select params={{ writingPadId: writingPadId }} />;
+export default function Page() {
+  return <Select />;
 }
