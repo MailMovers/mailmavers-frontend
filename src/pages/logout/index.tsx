c813d166c9589630e61d49a3b74389d56129fc43
xeo <xeo.seo@companywe.co.kr>
2024-05-10 17:03:17 +0900

    fix : 로그아웃 > 사용자 정보 초기화

--- src/pages/logout/index.tsx
+++ src/pages/logout/index.tsx
@@ -4,15 +4,18 @@ import { Spin } from 'antd';
 import { removeToken } from '@/common/axio-interceptor';
 import { useResetRecoilState, useSetRecoilState } from 'recoil';
 import { tokenAtom } from '@/recoil/auth/atom';
+import { userInfoAtom } from '@/recoil/mypage/atom';
 
 export default function Logout() {
   const setToken = useSetRecoilState(tokenAtom);
+  const resetUserInfo = useResetRecoilState(userInfoAtom);
 
   const router = useRouter();
 
   useEffect(() => {
     removeToken().then(() => {
       setToken(null);
+      resetUserInfo();
       router.push('/?status=expire');
     });
   }, []);
