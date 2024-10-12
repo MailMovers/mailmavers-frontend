088193c10cf3a681ac0625f3acf43a11d0eab00a
Corilla <leeyunje96@gmail.com>
2024-10-12 18:06:47 +0900

    [fix]: 이메일 중복확인 api 엔드포인트 변경

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -45,7 +45,7 @@ export function postEmailAuthenticationNumber(
 }
 
 export function postUserEmailCheck(email: string) {
-  return instance.post('/user/email-check', { email });
+  return instance.post('/users/email-duplicate-check', { email });
 }
 
 export function googleSocailLogin() {
