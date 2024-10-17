1e579b1c0814b97bfdaf4aeff4a1e3123b861d01
handymansodo <handymansodo@gmail.com>
2024-10-17 18:59:48 +0900

    [feat]: 편지 불러오기 연결

--- src/components/products/list.tsx
+++ src/components/products/list.tsx
@@ -1,58 +1,95 @@
 import * as S from "./style/productsList.styles";
-import * as SM from './style/menubar.styles';
-import InfiniteScroll from 'react-infinite-scroller';
-import { useState, useEffect } from 'react';
-import { product } from './mocks';
+import * as SM from "./style/menubar.styles";
+import InfiniteScroll from "react-infinite-scroller";
+import { useState, useEffect } from "react";
 import { AiOutlineMenu, AiOutlineFile } from "react-icons/ai";
 import { IoColorPaletteOutline } from "react-icons/io5";
 import { SiTinyletter } from "react-icons/si";
 import { FaStar } from "react-icons/fa";
-import { Item, PProductListProps } from './types';
-import  BannerContainer  from './slide-banner'
-import  { IconStyle }  from './style/menubar.styles'
-import { useMoveToPage } from "@/hooks/useMoveToPage"
-
+import { Item, PProductListProps } from "./types";
+import BannerContainer from "./slide-banner";
+import { IconStyle } from "./style/menubar.styles";
+import { useMoveToPage } from "@/hooks/useMoveToPage";
+import { fetchProducts } from "./axios";
 
 const menuItems = [
-    { icon: <IconStyle><AiOutlineMenu id="burgerIcon" /></IconStyle>, category: '전체보기' },
-    { icon: <IconStyle><IoColorPaletteOutline id="colorLetter"/></IconStyle>, category: '컬러편지지' },
-    { icon: <IconStyle><AiOutlineFile id="none" /></IconStyle>, category: '일반편지지' },
-    { icon: <IconStyle><SiTinyletter id="best"/></IconStyle>, category: '인기순' },
-    { icon: <IconStyle><FaStar id="star" /></IconStyle>, category: '리뷰순' },
-  ];
-  
+  {
+    icon: (
+      <IconStyle>
+        <AiOutlineMenu id="burgerIcon" />
+      </IconStyle>
+    ),
+    category: "전체보기",
+  },
+  {
+    icon: (
+      <IconStyle>
+        <IoColorPaletteOutline id="colorLetter" />
+      </IconStyle>
+    ),
+    category: "컬러편지지",
+  },
+  {
+    icon: (
+      <IconStyle>
+        <AiOutlineFile id="none" />
+      </IconStyle>
+    ),
+    category: "일반편지지",
+  },
+  {
+    icon: (
+      <IconStyle>
+        <SiTinyletter id="best" />
+      </IconStyle>
+    ),
+    category: "인기순",
+  },
+  {
+    icon: (
+      <IconStyle>
+        <FaStar id="star" />
+      </IconStyle>
+    ),
+    category: "리뷰순",
+  },
+];
+
 export default function ProductList(props: PProductListProps) {
   const [items, setItems] = useState<Item[]>([]);
   const [hasMore, setHasMore] = useState(true);
-  const [selectedCategory, setSelectedCategory] = useState('전체보기');
+  const [selectedCategory, setSelectedCategory] = useState("전체보기");
   const [page, setPage] = useState(0);
   const { onClickMoveToPage } = useMoveToPage();
 
-
   // 카테고리가 변경될 때 필터링된 아이템을 다시 불러옴
   useEffect(() => {
-    // 카테고리가 변경될 때 페이지와 아이템 초기화
-    setPage(0);
-    setItems([]);
+    const fetchData = async () => {
+      try {
+        const data = await fetchProducts(
+          0,
+          selectedCategory === "전체보기" ? undefined : selectedCategory
+        );
+        setItems(data);
+        setHasMore(data.length === 6);
+        setPage(1); // 페이지를 1로 초기화
+      } catch (error) {
+        console.error("Failed to fetch products:", error);
+      }
+    };
 
-    // 선택된 카테고리로 아이템 필터링
-    const filteredItems = selectedCategory === '전체보기' 
-      ? product 
-      : product.filter(item => item.category === selectedCategory);
-
-    // 첫 페이지 아이템만 불러오기
-    setItems(filteredItems.slice(0, 10));
-    setHasMore(filteredItems.length > 10); // 만약 10개 이하라면 더 불러올 것이 없으므로 false
-  }, [selectedCategory]);
+    fetchData();
+  }, [selectedCategory]); // selectedCategory가 변경될 때마다 useEffect 실행
 
   // 무한 스크롤로 더 많은 아이템을 불러오는 함수
   const loadMore = (page: number) => {
-    const filteredItems = selectedCategory === '전체보기' 
-      ? product 
-      : product.filter(item => item.category === selectedCategory);
+    const filteredItems =
+      selectedCategory === "전체보기"
+        ? items
+        : items.filter((item) => item.category === selectedCategory);
     const newItems = filteredItems.slice(page * 10, (page + 1) * 10);
 
-    setItems(prevItems => [...prevItems, ...newItems]);
+    setItems((prevItems) => [...prevItems, ...newItems]);
     setHasMore(filteredItems.length > (page + 1) * 10);
   };
 
@@ -66,9 +103,9 @@ export default function ProductList(props: PProductListProps) {
       <SM.MenuBarWrapper>
         <SM.MenuList>
           {menuItems.map(({ icon, category }) => (
-            <SM.MenuItem 
-              key={category} 
-              onClick={() => handleCategoryChange(category)} 
+            <SM.MenuItem
+              key={category}
+              onClick={() => handleCategoryChange(category)}
               isActive={selectedCategory === category}
             >
               {icon}
@@ -86,18 +123,21 @@ export default function ProductList(props: PProductListProps) {
       >
         <S.ProductList>
           {items.map((product: Item, id) => (
-            <S.ProductCard key={product.id} onClick={onClickMoveToPage(`/product/${id}`)}>
-              <S.ProductImage src={product.productImage}/>
+            <S.ProductCard
+              key={product.id}
+              onClick={onClickMoveToPage(`/product/${id}`)}
+            >
+              <S.ProductImage src={product.padImg1} />
               <S.ProductInfo>
-                <S.ProductName>{product.productTitle}</S.ProductName>
-                <S.ProductDescription>{product.productSubTitle}</S.ProductDescription>
+                <S.ProductName>{product.name}</S.ProductName>
+                <S.ProductDescription>{product.name}</S.ProductDescription>
                 <S.ProductPrice>
-                  <span className="category">삼풍종류: {product.category}</span>
-                  <span className="productPrice">{product.productPrice}</span>
+                  <span className="category">상품종류: {product.category}</span>
+                  <span className="productPrice">{product.price} 원</span>
                   <span className="sendDate">내일 모레 도착 예정</span>
                 </S.ProductPrice>
                 <S.ProductRating>
-                  {product.productStarRate} ({product.productReviewCount} 리뷰)
+                  {product.avgScore} ({product.reviewCount} 리뷰)
                 </S.ProductRating>
               </S.ProductInfo>
             </S.ProductCard>
--- src/components/products/types.ts
+++ src/components/products/types.ts
@@ -1,18 +1,17 @@
 export interface Item {
-    id: number;
-    category: string;
-    productImage: string;
-    productTitle: string;
-    productSubTitle: string;
-    productPrice: string;
-    productStarRate: string;
-    productReviewCount: string;
-  }
-  
+  id: number;
+  category: string;
+  padImg1: string;
+  name: string;
+  // name: string;
+  price: string;
+  avgScore: string;
+  reviewCount: string;
+}
 
 export interface PProductListProps {
-    onCategoryChange: (category: string) => void;
-    selectedCategory: string;
-    items: Item[];
-    isActive: boolean
+  onCategoryChange: (category: string) => void;
+  selectedCategory: string;
+  items: Item[];
+  isActive: boolean;
 }
